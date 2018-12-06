9445f2377e05dec62b81146285282461578f2c22
Teemu Laitila <squigothic@gmail.com>
2018-12-06 13:35:13 +0200

    axios moved to separate module

--- src/App.js
+++ src/App.js
@@ -1,48 +1,34 @@
-import React, { Component } from 'react';
-import axios from 'axios'
-import Filter from './components/Filter'
-import AllCountries from './components/AllCountries'
+import React from 'react'
+import measurementService from './services/measurements'
 
-class App extends Component {
-  constructor() {
-    super()
+class App extends React.Component {
+  constructor(props) {
+    super(props)
     this.state = {
-      maat: [],
-      filter: '',
+      measurements: []
     }
   }
 
   componentDidMount() {
-    axios
-      .get('https://restcountries.eu/rest/v2/all')
+    measurementService
+      .getAll()
       .then(response => {
-        console.log(response.data)
-        this.setState({ maat: response.data })
+        this.setState({ measurements: response.data })
+        return response.data
       })
-  }
-
-  handleFilter = (event) => {
-    console.log(event.target.value)
-    this.setState({ filter: event.target.value})
-  }
-
-  setFilter = (newFilter) => {
-    return () => {
-      this.setState({ filter: newFilter})
-    }
+      .then(data => {
+        console.log(data)
+      })
   }
 
   render() {
     return (
       <div>
-        <h1>Maa-ohjelma</h1>
-        <Filter filter={this.state.filter} handleFilter={this.handleFilter} />
-        <div>
-          <AllCountries countryList={this.state.maat} filter={this.state.filter} handleClick={this.setFilter} />
-        </div>
-      </div>
+        <h1>Ruuvifrontend</h1>
+      </div >
     )
   }
 }
 
-export default App;
+
+export default App
