19da6ff5ea8e44741c4017299e379ff63dc4f907
Teemu <squigothic@gmail.com>
2019-04-24 20:41:56 +0300

    baseUrl now points to actual API

--- src/services/measurements.js
+++ src/services/measurements.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 require('dotenv').config()
 
-const baseUrl = 'http://localhost:3001/measurements'
+const baseUrl = 'https://subdomain.marakassi.com/measurements'
 
 const getAll = () => {
   console.log(`Tehdään pyyntö osoitteeseen ${baseUrl}`)
