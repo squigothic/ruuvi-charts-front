92fda13f9cdf85dfc3d7473aff9199bac4f8c800
Katri Vuori <ktrvuori@gmail.com>
2020-11-19 14:47:42 +0200

    adding one space

--- src/services/loginservice.ts
+++ src/services/loginservice.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-//const baseUrl = 'https://dry-ocean-51491.herokuapp.com/login';
+// const baseUrl = 'https://dry-ocean-51491.herokuapp.com/login';
 const baseUrl = 'https://nameless-river-83647.herokuapp.com/login';
 
 const login = async ({ username, password }: { username: string; password: string }) => {
