46bc2ebc8434f8e99eeb8791177dccbab62fac87
Katri Vuori <ktrvuori@gmail.com>
2021-01-29 14:04:24 +0200

    show error if no measurement data

--- src/components/frontpage/Frontpage.tsx
+++ src/components/frontpage/Frontpage.tsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import RuuviChart from '../RuuviChart';
 import Datedisplay from '../timepicker/Datedisplay';
 import { RootState } from '../../types';
+import Loading from '../Loading';
 
 const Frontpage = () => {
   const measurements = useSelector((state: RootState) => state.measurements.recurring);
@@ -11,13 +12,17 @@ const Frontpage = () => {
   return (
     <>
       <Datedisplay currentTimeperiod={currentTimeperiod} />
-      {measurements.map((tag) => (
-        <RuuviChart
-          key={tag[0].data.friendlyname}
-          recurringMeasurements={tag.map((measurement) => measurement.data)}
-          tagFriendlyName={tag[tag.length - 1].data.friendlyname}
-        />
-      ))}
+      {measurements.length !== 0 ? (
+        measurements.map((tag) => (
+          <RuuviChart
+            key={tag[0].data.friendlyname}
+            recurringMeasurements={tag.map((measurement) => measurement.data)}
+            tagFriendlyName={tag[tag.length - 1].data.friendlyname}
+          />
+        ))
+      ) : (
+        <Loading text="No data available for selected timeperiod" />
+      )}
     </>
   );
 };
